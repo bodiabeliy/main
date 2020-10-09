69681971e0061973e6b379f39c00c90c6e968e0b
Bogdan <53011638+bodiabeliy@users.noreply.github.com>
2020-10-09 14:48:22 +0300

    mainpage

---
+++ index.js
@@ -0,0 +1,23 @@
+$('.ar5').click(() => {
+ $('.detal_info').animate({"opacity": 1}, 1200)
+});
+
+$('.ar4').click(() => {
+ $('.detal_info').animate({"opacity": 1}, 1200)
+});
+
+$('.ar3').click(() => {
+ $('.detal_info').animate({"opacity": 1}, 1200)
+});
+
+$('.ar2').click(() => {
+ $('.detal_info').animate({"opacity": 1}, 1200)
+});
+
+$('.ar1').click(() => {
+ $('.detal_info').animate({"opacity": 1}, 1200)
+});
+
+$('.top').click(() => {
+ $('.detal_info').animate({"height": 0}, 600).fadeOut(0)
+});
